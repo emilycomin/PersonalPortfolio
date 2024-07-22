de324ced547febc5afc6a64774f789288392445c
Emily Comin <emilycomin@hotmail.com>
2024-07-22 15:22:49 -0300

    adicionando modais para os projetos

---
+++ script.js
@@ -0,0 +1,38 @@
+let wave100 = document.getElementById("wave100");
+let wave70 = document.getElementById("wave70");
+let wave40 = document.getElementById("wave40");
+let saudation = document.getElementById("saudation");
+let emilyComin = document.getElementById("emilyComin");
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    saudation.style.marginTop = value * 1.5 + 'px';
+    emilyComin.style.right = value * -1.0 + 'px';
+    // emilyComin.style.top = value * -1.0 + 'px';
+    // wave40.style.bottom = value * 0.5 + 'px';
+    wave40.style.bottom = value * -.5 + 'px'
+})
+
+var modalReviva = document.getElementById("modalReviva");
+var modalAgromari = document.getElementById("modalAgromari");
+
+    // O botão Update abre uma Dialog
+    document.getElementById("openButton").onclick = (e) => {
+        e.preventDefault();
+        modalReviva.showModal();
+    }
+    document.getElementById("closeButton").onclick = (e) =>{
+        e.preventDefault();
+        modalReviva.close();
+    }
+    document.getElementById("openButtonAgromari").onclick = (e)=>{
+        e.preventDefault();
+        modalAgromari.showModal();
+    }
+    document.getElementById("closeButtonAgromari").onclick = (e) =>{
+        e.preventDefault();
+        modalAgromari.close();
+    }
+
+
+    
