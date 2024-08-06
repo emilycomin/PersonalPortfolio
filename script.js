a82046c7aefc2fa0200f5b6b0621224c56b90294
Emily Comin <emilycomin@hotmail.com>
2024-08-05 22:10:23 -0300

    finalizado resposividade mobile

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let wave100 = document.getElementById("wave100");
 let wave70 = document.getElementById("wave70");
 let wave40 = document.getElementById("wave40");
 let saudation = document.getElementById("saudation");
@@ -6,40 +5,8 @@ let emilyComin = document.getElementById("emilyComin");
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    saudation.style.marginTop = value * 1.5 + 'px';
-    emilyComin.style.right = value * -1.0 + 'px';
-    wave70.style.bottom = value * - .3 + 'px';
-    wave40.style.bottom = value * -1 + 'px'
+    saudation.style.marginTop = value * 0.5 + 'px';
+    emilyComin.style.right = - value * 0.5 + 'px';
+    wave70.style.bottom = - value *0.2 + 'px';
+    wave40.style.bottom = - value + 'px';
 })
-
-var modalReviva = document.getElementById("modalReviva");
-var modalAgromari = document.getElementById("modalAgromari");
-var modalDhouse = document.getElementById("modalDhouse");
-
-    // O botão Update abre uma Dialog
-    document.getElementById("openButton").onclick = (e) => {
-        e.preventDefault();
-        modalReviva.showModal();
-    }
-    document.getElementById("closeButton").onclick = (e) =>{
-        e.preventDefault();
-        modalReviva.close();
-    }
-    document.getElementById("openButtonAgromari").onclick = (e) => {
-        e.preventDefault();
-        modalAgromari.showModal();
-    }
-    document.getElementById("closeButtonAgromari").onclick = (e) =>{
-        e.preventDefault();
-        modalAgromari.close();
-    }
-    document.getElementById("openModalDhouse").onclick = (e) => {
-        e.preventDefault();
-        modalDhouse.showModal();
-    }
-    document.getElementById("closeButtonDhouse").onclick = (e) => {
-        e.preventDefault();
-        modalDhouse.close();
-    }
-
-    
